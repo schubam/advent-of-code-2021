9661540073c92c53929864172e27ad483414ea92
Marc Schuba <me@schubam.com>
2021-12-20 10:58:22 +0100

    feat: first half of day 3 part 2 done

--- day_03/day3_test.go
+++ day_03/day3_test.go
@@ -20,7 +20,7 @@ func TestSolve(t *testing.T) {
 01010`
 
 	want := 198
-	got := Solve(input)
+	got := Solve(input).part1
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
 	}
@@ -35,5 +35,38 @@ func TestCalculate(t *testing.T) {
     want := map[int]int{0:2, 1:1}
 	if got[0][0] != want[0] || got[0][1] != want[1] {
 		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func stringSlicesEqual(a, b []string) bool {
+    if len(a) != len(b) {
+        return false
+    }
+    for i, v := range a {
+        if v != b[i] {
+            return false
+        }
+    }
+    return true
+}
+
+func TestDrillDown(t *testing.T) {
+	input := strings.Split(strings.TrimSpace(`00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010`), "\n")
+
+    got := DrillDown(input)
+    want := "10111"
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
 	}
 }
--- day_03/main.go
+++ day_03/main.go
@@ -9,6 +9,14 @@ import (
 
 type calculation map[int]int
 
+func (c calculation) winner() string {
+	if c[1] >= c[0] {
+		return "1"
+	} else {
+		return "0"
+	}
+}
+
 func CalculateBits(words []string) []calculation {
 	l := len(words[0])
 	result := []calculation{}
@@ -16,7 +24,7 @@ func CalculateBits(words []string) []calculation {
 	for pos := 0; pos < l; pos++ {
 		c := calculation{0: 0, 1: 0}
 		for _, w := range words {
-            //fmt.Printf("pos: %d, w: %s, w[pos]: %s, c: %v\n", pos, w, string(w[pos]), c)
+			//fmt.Printf("pos: %d, w: %s, w[pos]: %s, c: %v\n", pos, w, string(w[pos]), c)
 			if string(w[pos]) == "1" {
 				c[1]++
 			} else {
@@ -28,9 +36,9 @@ func CalculateBits(words []string) []calculation {
 	return result
 }
 
-type appendOne func(a, b int) bool
+type comparer func(a, b int) bool
 
-func forEach(cs []calculation, f appendOne) int {
+func eachLine(cs []calculation, f comparer) int {
 	var gamma string
 
 	for _, c := range cs {
@@ -50,29 +58,67 @@ func forEach(cs []calculation, f appendOne) int {
 }
 
 func epsilonRate(cs []calculation) int {
-	return forEach(cs, func(a, b int) bool {
+	return eachLine(cs, func(a, b int) bool {
 		return a > b
 	})
 }
 
 func gammaRate(cs []calculation) int {
-	return forEach(cs, func(a, b int) bool {
+	return eachLine(cs, func(a, b int) bool {
 		return a < b
 	})
 }
 
-func Solve(input string) int {
+func DrillDown(lines []string) string {
+	result := lines
+
+    wordLen := len(lines[0])
+    i := 0
+    for {
+        if (i >= wordLen) {
+            break
+        }
+        cs := CalculateBits(result)
+
+        var r2 []string
+        c := cs[i]
+
+		for _, l := range result {
+			if string(l[i]) == c.winner() {
+                r2 = append(r2, l)
+			}
+        }
+        fmt.Printf("i: %d, c: %v, winner: %s, results: %v, r2: %v\n", i, c, c.winner(), result, r2)
+
+        result = r2
+		if len(result) == 1 {
+			break
+		}
+        i++
+    }
+
+	return result[0]
+}
+
+type result struct {
+	part1 int
+	part2 int
+}
+
+func Solve(input string) result {
 	lines := strings.Split(input, "\n")
-    //fmt.Println(lines)
+	//fmt.Println(lines)
 	c := CalculateBits(lines)
-	return gammaRate(c) * epsilonRate(c)
+	r := result{}
+	r.part1 = gammaRate(c) * epsilonRate(c)
+	return r
 }
 
 func main() {
-    data, err := ioutil.ReadFile("data.txt")
+	data, err := ioutil.ReadFile("data.txt")
 	if err != nil {
 		fmt.Println("error: ", err)
 	}
-    content := strings.TrimSpace(string(data))
+	content := strings.TrimSpace(string(data))
 	fmt.Println(Solve(content))
 }
