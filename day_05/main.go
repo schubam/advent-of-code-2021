1c7e6beef1721f789ca56b886ced7205b2e5b519
Marc Schuba <me@schubam.com>
2021-12-27 22:01:12 +0100

    feat: day 5 part 2

--- day_05/main.go
+++ day_05/main.go
@@ -9,6 +9,44 @@ import (
 	"strings"
 )
 
+func render(m map[Point]int) {
+	var maxX, maxY, minX, minY int
+	for p := range m {
+		if p.x > maxX {
+			maxX = p.x
+		}
+		if p.x < minX {
+			minX = p.x
+		}
+		if p.y > maxY {
+			maxY = p.y
+		}
+		if p.y < minY {
+			minY = p.y
+		}
+	}
+
+	width := maxX - minX + 1
+	height := maxY - minY + 1
+
+	fmt.Printf("width: %d, height: %d\n", width, height)
+
+	for j := minY; j <= maxY; j++ {
+		for i := minX; i <= maxX; i++ {
+			if math.Mod(float64(i), float64(width)) == 0 {
+				fmt.Printf("\n")
+			}
+			value, ok := m[Point{i, j}]
+			if !ok {
+				fmt.Printf(".")
+			} else {
+				fmt.Printf("%d", value)
+			}
+		}
+	}
+	fmt.Printf("\n")
+}
+
 type Point struct {
 	x, y int
 }
@@ -17,19 +55,16 @@ type Line struct {
 	a, b Point
 }
 
-func (l Line) coordinates() []Point {
+func (l Line) Coordinates(isV1 bool) []Point {
 	var coords []Point
-	if l.a.x == l.b.x || l.a.y == l.b.y {
-		ax := float64(l.a.x)
-		bx := float64(l.b.x)
-		ay := float64(l.a.y)
-		by := float64(l.b.y)
-		deltaX := math.Abs(ax - bx)
-		deltaY := math.Abs(ay - by)
+	ax := float64(l.a.x)
+	bx := float64(l.b.x)
+	ay := float64(l.a.y)
+	by := float64(l.b.y)
+	deltaX := math.Abs(ax - bx)
+	deltaY := math.Abs(ay - by)
 
-		// 0 -> 5
-		// 0,1,2,3,4,5 | six elements
-		//
+	if l.a.x == l.b.x || l.a.y == l.b.y {
 		if deltaX > 0 {
 			for offset := 0; offset <= int(deltaX); offset++ {
 				coords = append(coords, Point{int(math.Min(ax, bx)) + offset, l.a.y})
@@ -41,12 +76,22 @@ func (l Line) coordinates() []Point {
 				coords = append(coords, Point{l.a.x, int(math.Min(ay, by)) + offset})
 			}
 		}
+	} else {
+		if !isV1 {
+			// diagonal
+			var offset float64
+			for offset = 0; offset <= deltaY; offset++ {
+				signX := (ax - bx) / deltaX * -1
+				signY := (ay - by) / deltaY * -1
+				coords = append(coords, Point{int(ax + signX*offset), int(ay + signY*offset)})
+			}
+		}
 	}
 
 	return coords
 }
 
-func Solve(input string) int {
+func Solve(input string) (int, int) {
 	regex := regexp.MustCompile(`(\d+),(\d+) -> (\d+),(\d+)`)
 	var lines []Line
 	for _, line := range strings.Split(input, "\n") {
@@ -65,21 +110,36 @@ func Solve(input string) int {
 		}
 	}
 
-	overlap := make(map[Point]int)
+	overlap_V1 := make(map[Point]int)
 	for _, line := range lines {
-		for _, point := range line.coordinates() {
-			overlap[point] += 1
+		for _, point := range line.Coordinates(true) {
+			overlap_V1[point] += 1
 		}
 	}
-	//fmt.Printf("overlap: %v\n", overlap)
+	//fmt.Printf("overlap: %v\n", overlap_V1)
+	//render(overlap_V1)
 
-	var count int
-	for _, value := range overlap {
-		if value > 1 {
-			count++
+	overlap_V2 := make(map[Point]int)
+	for _, line := range lines {
+		for _, point := range line.Coordinates(false) {
+			overlap_V2[point] += 1
 		}
 	}
-	return count
+	//fmt.Printf("overlap: %v\n", overlap_V2)
+	render(overlap_V2)
+
+	var count_V1, count_V2 int
+	for _, value := range overlap_V1 {
+		if value > 1 {
+			count_V1++
+		}
+	}
+	for _, value := range overlap_V2 {
+		if value > 1 {
+			count_V2++
+		}
+	}
+	return count_V1, count_V2
 }
 
 func main() {
--- day_05/main_test.go
+++ day_05/main_test.go
@@ -1,23 +1,40 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSolve(t *testing.T) {
 	input := `0,9 -> 5,9
-8,0 -> 0,8
-9,4 -> 3,4
-2,2 -> 2,1
-7,0 -> 7,4
-6,4 -> 2,0
-0,9 -> 2,9
-3,4 -> 1,4
-0,0 -> 8,8
-5,5 -> 8,2`
+    8,0 -> 0,8
+    9,4 -> 3,4
+    2,2 -> 2,1
+    7,0 -> 7,4
+    6,4 -> 2,0
+    0,9 -> 2,9
+    3,4 -> 1,4
+    0,0 -> 8,8
+    5,5 -> 8,2`
 
-	got := Solve(input)
-	want := 5
+	got1, got2 := Solve(input)
+	want1 := 5
+	want2 := 12
 
-	if got != want {
-		t.Errorf("error, got %d, want %d", got, want)
+	if got1 != want1 {
+		t.Errorf("error, got %d, want %d", got1, want1)
+	}
+
+	if got2 != want2 {
+		t.Errorf("error, got %d, want %d", got2, want2)
+	}
+}
+
+func TestCoordinates(t *testing.T) {
+	line := Line{Point{5, 5}, Point{8, 2}}
+	got := line.Coordinates(false)
+	want := []Point{{5, 5}, {6, 4}, {7, 3}, {8, 2}}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("error, got %v, want %v", got, want)
 	}
 }
